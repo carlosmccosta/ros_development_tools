6994b5dcddb748556ac28772bd5536a86cc73415
Carlos Costa <carloscosta.cmcc@gmail.com>
2016-03-09 11:41:23 +0000

    Added setup of ros environment and catkin workspace. Added script to install dev apps after ubuntu installation.

--- catkin/create_catkin_tools_ws.sh
+++ catkin/create_catkin_tools_ws.sh
@@ -10,9 +10,15 @@ catkin_ws_dir=${1:-'catkin_ws'}
 
 mkdir -p "$HOME/${catkin_ws_dir}/src"
 cd "$HOME/${catkin_ws_dir}"
-catkin config --init --isolate-devel --no-install --extend /opt/ros/jade
+catkin config --init --no-install --extend /opt/ros/jade
+catkin config --profile debug -x _debug --cmake-args -DCMAKE_BUILD_TYPE=Debug
+catkin config --profile release -x _release --cmake-args -DCMAKE_BUILD_TYPE=Release
+catkin config --profile release_with_debug_info -x _release_with_debug_info --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+catkin profile set debug
+catkin config --append-args --cmake-args -DCMAKE_CXX_FLAGS="-Wall -W -Wno-unused-parameter"
+
 
 echo "\n\n"
 echo "----------------------------------------------------------------------------------------------------"
-echo ">>>>> Eclipse catkin_ws projects in ~/${catkin_ws_dir}/build"
+echo ">>>>> catkin workspace initilized"
 echo "----------------------------------------------------------------------------------------------------"
